b97f07213955ea1c9e8743f9a664c2766cd1da36
albud187 <alfa.budiman@outlook.com>
2021-01-21 07:12:37 -0400

    update

--- project_safety_zone/app_api/views.py
+++ project_safety_zone/app_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.core import mail
+from django.utils.html import strip_tags
+from django.conf import settings
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
