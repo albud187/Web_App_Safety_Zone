33130088be8b303a6025d11d1fea8c174a25acf4
albud187 <alfa.budiman@outlook.com>
2021-01-22 22:17:40 -0400

    update

--- project_safety_zone/app_reports/models.py
+++ project_safety_zone/app_reports/models.py
@@ -6,4 +6,5 @@ class FlightSafetyReport(models.Model):
     location = models.CharField(max_length=100)
     description = models.TextField()
     date_time = models.CharField(max_length=30)
-    contact = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=100)
+    contact_email = models.CharField(max_length=100)
